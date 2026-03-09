22699e03502dca08ea9963fb9bb2cf274a450bde
Danil_Moiseenko <danil.moiseenko@icloud.com>
2026-03-09 12:34:13 +0500

    Refactor Recognizer and WhisperSpeechRecognitionModel classes for improved initialization and error handling

--- src/services/ml_service/models/recognizer.py
+++ src/services/ml_service/models/recognizer.py
@@ -2,20 +2,21 @@ from typing import Union, Any
 
 from src.config.services.ml_config import settings, Settings
 from src.services.ml_service.models.recognizers.base import BaseRecognizer
+from .recognizers.openai_whisper import WhisperSpeechRecognitionModel
 from ..utils import Response 
 
 MODELS = {
     'whisper': ['tiny', 'base', 'small', 'medium', 'large']
 }
 
-class Recognizer:
+class Recognizer(BaseRecognizer):
     def __init__(self, settings: Settings = settings):
+        super().__init__(name="Recognizer")
         self.settings: Settings = settings
 
         if self.settings.RECOGNIZER_TYPE == 'whisper':
             if self.settings.RECOGNIZER_NAME not in MODELS['whisper']:
                 raise ValueError(f"Unsupported Whisper model: {self.settings.RECOGNIZER_NAME}")
-            from src.services.ml_service.models.recognizers.openai_whisper import WhisperSpeechRecognitionModel
             self.model = WhisperSpeechRecognitionModel(model_name=self.settings.RECOGNIZER_NAME, cache_dir=self.settings.MODEL_CACHE_DIR)
         
     
